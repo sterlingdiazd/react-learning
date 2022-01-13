d4dc5a247af32dd0e99914bd485cafeb8586aa23
Sterling Michel Diaz de Oleo <sdiaz@arssenasa.gov.do>
2022-01-13 18:37:08 -0400

    Inline map result

--- src/components/App.js
+++ src/components/App.js
@@ -50,9 +50,14 @@ class App extends Component {
 
 function NumberList(props) {
     const numbers = props.numbers
-    const listItems = numbers.map((number) => <li key={number.toString()}>{number}</li>)
     return (
-        <ul>{listItems}</ul>
+        <ul>{numbers.map((number) => <ListItem key={number.toString()} value={number} />)}</ul>
+    )
+}
+
+function ListItem(props) {
+    return (
+        <li> {props.value}</li >
     )
 }
 
